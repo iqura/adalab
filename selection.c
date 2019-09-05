5e31377aba9af4c3f6386499b1f0399dba9c7010
iqura <54399092+iqura@users.noreply.github.com>
2019-09-05 16:12:24 +0530

    Update selection.c

--- selection.c
+++ selection.c
@@ -1,67 +1,83 @@
-#include <stdio.h> 
+import random
+arr=[]
+def selection(a,n):
+	count =0
+	for i in range(0,n-1):
+		max_index = i
+		for j in range(i+1,n):
+			count = count+1
+			if a[j]>a[max_index]:
+				max_index =j
+				
+		temp = a[max_index]
+		a[max_index]=a[i]
+		a[i] = temp
+	print("selection:",count)
 
-void swap(int *xp, int *yp) 
-{ 
+def bubble(a,n):
+	count =0
+	for i in range(0,n-1):
+		for j in range(0,n-1-i):
+			if a[j]>a[j+1]:
+				temp = a[j]
+				a[j]=a[j+1]
+				a[j+1] = temp
+				count = count+1
+	print("bubble: ", count)
 
-    int temp = *xp; 
+count =0
+def mergeSort(arr): 
+	global count
+	if len(arr) >1: 
+		mid = len(arr)//2 
+		L = arr[:mid] 
+		R = arr[mid:]
 
-    *xp = *yp; 
+		mergeSort(L) 
+		mergeSort(R)
 
-    *yp = temp; 
-} 
-
-  
-
-void selectionSort(int arr[], int n) 
-{ 
-
-    int i, j, min_idx,count=0; 
-
-    for (i = 0; i < n-1; i++) 
-
-    { 
-        min_idx = i; 
-
-        for (j = i+1; j < n; j++) 
-	{
-	  count++;
-
-          if (arr[j] < arr[min_idx]) 
-
-            min_idx = j; 
-
-        swap(&arr[min_idx], &arr[i]); 
-	}
-    } 
-     printf("the comparision:%d\n",count);
-} 
+		i = j = k = 0		
+		
+		while i < len(L) and j < len(R):
+			count = count+1 
+			if L[i] < R[j]: 
+				
+				arr[k] = L[i] 
+				i+=1
+			else: 
+				arr[k] = R[j] 
+				j+=1
+			k+=1		
+		 
+		while i < len(L): 
+			arr[k] = L[i] 
+			i+=1
+			k+=1
+		
+		while j < len(R): 
+			arr[k] = R[j] 
+			j+=1
+			k+=1
+	
 
 
-void printArray(int arr[], int size) 
-{ 
+def printList(arr): 
+	for i in range(len(arr)):		 
+		print(arr[i],end=" ") 
+	print() 
 
-    int i; 
 
-    for (i=0; i < size; i++) 
 
-        printf("%d\n", arr[i]); 
+n=6
+for i in range(0,n):
+	arr.append(random.randint(0,100))
 
-    printf("\n"); 
-} 
+arr1=arr
+selection(arr1,n)
+bubble(arr,n)		
 
-int main()
-{
-	int arr[10],n,i;
-	printf("enter the size of array");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		printf("enter the %dth element:",i+1);
-		scanf("%d",&arr[i]);
-	}
-	selectionSort(arr, n); 
-	printf("Sorted array: \n"); 
-	printArray(arr, n); 
-	return 0;
-}
+arr2 = [12, 11, 13, 5, 6, 7] 
+mergeSort(arr2)
+print(count) 
+
 
