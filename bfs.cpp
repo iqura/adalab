cbc916c2e48731b44134ebf7790ff868a63e755c
iqura <54399092+iqura@users.noreply.github.com>
2019-09-12 15:46:28 +0530

    Add files via upload

---
+++ bfs.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int a[10][10],q[10],visited[10],n,i,j,f=0,r=-1;
+void bfs(int v) {
+for (i=1;i<=n;i++)
+if(a[v][i] && !visited[i])
+q[++r]=i;
+if(f<=r) {
+visited[q[f]]=1;
+bfs(q[f++]);
+}
+}
+int main() 
+{
+int v;
+printf("\n Enter the number of vertices:");
+scanf("%d",&n);
+for (i=1;i<=n;i++) 
+{
+q[i]=0;
+visited[i]=0;
+}
+printf("\n Enter graph in matrix:\n");
+for (i=1;i<=n;i++)
+{
+for (j=1;j<=n;j++)
+scanf("%d",&a[i][j]);
+}
+printf("\n Enter the starting vertex:");
+scanf("%d",&v);
+bfs(v);
+printf("\n The node which are reachable are:\n");
+for (i=1;i<=n;i++)
+if(visited[i])
+{
+printf("%d\t",i); 
+printf("\n");
+}
+else
+printf("\n Bfs is not possible\n");
+return 0;
+}
